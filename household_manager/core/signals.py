70eb3edf80c07384a01c9d8ece5c088dda256a69
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-21 14:31:45 +0200

    created siglnal for automatic notification creation

---
+++ household_manager/core/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Chore, Notification
+
+@receiver(post_save, sender=Chore)
+def create_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.assigned_to.user,
+            message=f"You have been assigned a new chore: {instance.title}",
+            family=instance.assigned_to.family,
+            chore=instance,
+        )
