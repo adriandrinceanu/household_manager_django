33520d6c403f39fc008ac91c6a1b6682b65c0df7
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-21 14:18:17 +0200

    created forms

---
+++ household_manager/core/forms.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import Member, Family, Chore, Budget, Expense
+
+class MemberCreationForm(UserCreationForm):
+    phone = forms.CharField(max_length=150, required=True)
+    profile_pic = forms.ImageField()
+
+    class Meta(UserCreationForm.Meta):
+        model = Member
+        fields = UserCreationForm.Meta.fields + ('phone', 'profile_pic',)
+        
+class FamilyCreationForm(forms.ModelForm):
+    class Meta:
+        model = Family
+        fields = ['name', 'profile_pic']
+        
+class FamilyMemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = ['name', 'email', 'phone', 'profile_pic']
+        
+        
+class ChoreCreationForm(forms.ModelForm):
+    class Meta:
+        model = Chore
+        fields = ['title', 'description', 'assigned_to']
+
+class BudgetCreationForm(forms.ModelForm):
+    class Meta:
+        model = Budget
+        fields = ['amount', 'category']
+
+class ExpenseCreationForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['amount', 'description', 'category', 'created_by']
