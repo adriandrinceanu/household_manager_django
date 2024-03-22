dc27effa9a1d41c2a5f96ed7a77c098017c6a6ef
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-22 14:32:32 +0200

    login and register fix. Family leader view

--- household_manager/core/views.py
+++ household_manager/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib.auth import login,logout,authenticate
-from .models import Chore, Family  
+from .models import Chore, Family, Member
+from django.contrib.auth.models import Group, User
 from .utils import create_chore  # Import the create_chore function
-from .forms import MemberCreationForm, FamilyMemberForm
+from .forms import MemberCreationForm
 
 def homepage(request):
     return render(request, 'home.html')
@@ -14,6 +15,12 @@ def logoutPage(request):
 def loginPage(request):
     if request.user.is_authenticated:
         username = request.user.username
+        if request.user.groups.filter(name='Family Leader').exists():
+            return redirect('family_leader', username=username)
+        elif request.user.groups.filter(name='Family Member').exists():
+            return redirect('family_member', username=username)
+        else:
+            return HttpResponse('You are not part of this Family group.')
     else:
         if request.method == "POST":
             username = request.POST.get('username')
@@ -21,25 +28,28 @@ def loginPage(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-        context = {}
+                if user.groups.filter(name='Family Leader').exists():
+                    return redirect('family_leader', username=username)
+                elif user.groups.filter(name='Family Member').exists():
+                    return redirect('family_member', username=username)
+                else:
+                    return HttpResponse('You are not part of this Family group.')
+        groups = Group.objects.prefetch_related('user_set')    
+        context = {'groups': groups}
         return render(request, 'login.html', context)
     
     
 def registerPage(request):
-    form=MemberCreationForm()
-    fam_form=FamilyMemberForm()
-    if request.method=='POST':
-        form=MemberCreationForm(request.POST)
-        fam_form=FamilyMemberForm(request.POST)
-        if form.is_valid() and fam_form.is_valid():
-            user=form.save()
-            customer=fam_form.save(commit=False)
-            customer.user=user 
-            customer.save()
+    if request.method == 'POST':
+        form = MemberCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
             return redirect('/')
+    else:
+        form = MemberCreationForm()
+        
     context={
         'form':form,
-        'cust_form':fam_form,
     }
     return render(request,'register.html',context)
 
@@ -56,3 +66,15 @@ def create_chore_view(request, family_id):
         create_chore(assigned_to, title, family, chore)  # Call the create_chore function
         # Handle successful creation and potentially redirect
     return render(request, 'create_chore.html')
+
+def familyLeader(request, username):
+    user = get_object_or_404(User, username=username)
+    member = Member.objects.get(user=user)
+    family_members = Member.objects.filter(family=member.family)
+    
+    context = {
+        'member': member,
+        'family': family_members,
+    }
+    
+    return render(request, 'family_leader.html', context)
